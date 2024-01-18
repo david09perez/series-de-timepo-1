081cd89e20120cd9817deca34310220708b11dcb
david09perez <123003000+david09perez@users.noreply.github.com>
2024-01-17 20:34:30 -0600

    Add files via upload

---
+++ series_de_tiempo1.1.r
@@ -0,0 +1,71 @@
+install.packages("MASS", version = "7.3-51.5")
+install.packages("ggplot2")
+install.packages("tidyverse")
+
+install.packages("tidyverse")
+
+library("tidyverse")
+
+1
+
+1+1
+
+x <- 1
+x
+
+x = 12
+x
+
+x
+
+msg <- "hola"
+msg
+
+2*3
+
+2**3
+
+2/2
+
+x <- c(0.5, 0.6)
+x <- c(TRUE, FALSE)
+x <- c(T, F)
+x <- c("a", "b", "c")
+x <- 9:29
+x <- c(1+0i, 2+4i)
+
+y <-c(1.7, "a") 
+y
+
+class(y)
+
+x <- vector("numeric", length = 10)
+x
+
+y <- c(TRUE, 2)
+paste("Valor de y:")
+y
+paste("clase de y:")
+class(y)
+
+m <- matrix(nrow = 2, ncol = 3)
+m
+
+dim(m)
+
+m <- matrix(1:6, nrow = 2, ncol = 3)
+m 
+
+x <- 1:3
+y <- 10:12
+cbind(x, y)
+
+rbind(x, y)
+
+x <- c(1, 2, NA, 10, 3)
+is.na(x)
+
+data(mpg)
+mpg
+
+
